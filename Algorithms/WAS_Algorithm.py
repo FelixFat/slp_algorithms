f24d9f56793a3090d6c95882b616934d23b9f50f
FelixFat <felixfat9@gmail.com>
2022-08-31 13:40:34 +0300

    FIX: Code commenting

--- Algorithms/WAS_Algorithm.py
+++ Algorithms/WAS_Algorithm.py
@@ -5,6 +5,10 @@ import pyransac3d as pyrsc
 
 
 class WAS_alg:
+    """
+    Weighted Area Search Algorithm for SLP search
+    """
+
     def __init__(self, in_data=np.array([])):
         self.data = in_data
 
@@ -14,9 +18,13 @@ class WAS_alg:
         self.slope = 0.0
         self.area = 0.0
         self.score = 0.0
-        self.point = [0.0, 0.0, 0.0]
+        self.point = []
 
     def call(self):
+        """
+        Weighted Area Search algorithm call
+        :return: None
+        """
         point_cloud = self.data.copy()
 
         while len(point_cloud) >= 0.3 * len(self.data):
@@ -28,11 +36,16 @@ class WAS_alg:
                 area = self.zone_area(in_pc=cluster)
                 score = self.zone_estimate(in_area=area, in_slope=slope)
                 if score > self.score:
-                    self.point = self.point_definition(in_pc=cluster)
+                    self.point = self.point_determination(in_pc=cluster)
             point_cloud = np.delete(point_cloud, inliers, axis=0)
+        return None
 
     def plane_detection(self, in_pc):
-        # RANSAC realization
+        """
+        Plane detection in point cloud with RANSAC algorithm
+        :param in_pc: Input point cloud
+        :return: Plane equation, Inliers points
+        """
         plane = pyrsc.Plane()
         equation, inliers = plane.fit(
             pts=in_pc,
@@ -43,12 +56,12 @@ class WAS_alg:
 
         return equation, inliers
 
-    def zone_clustering(self, in_pc):
-        # Kd-tree + euclidean clustering
-        return []
-
     def zone_slope(self, in_eq):
-        # Slope
+        """
+        Zone slope calculation
+        :param in_eq: Input equation of plane
+        :return: Zone slope
+        """
 
         # rad_phi = (a, b) / (magnitude(a) * magnitude(b))
         # grad_phi = abs(arccos(rad_phi) * 180 / PI)
@@ -62,14 +75,37 @@ class WAS_alg:
 
         return slope
 
+    def zone_clustering(self, in_pc):
+        """
+        Zones clustering in point cloud with Kd-tree and Euclidean clustering algorithms
+        :param in_pc: Input point cloud
+        :return: Zones points clusters
+        """
+        return []
+
     def zone_area(self, in_pc):
+        """
+        Zone area calculation
+        :param in_pc: Input point cloud
+        :return: Zone area
+        """
         return 0
 
     def zone_estimate(self, in_area, in_slope):
+        """
+        Landing zone estimation
+        :param in_area: Input zone area
+        :param in_slope: Input zone slope
+        :return: Estimate
+        """
         return 0.0
 
-    def point_definition(self, in_pc):
-        # Geometric median
+    def point_determination(self, in_pc):
+        """
+        Landing point determination with geometric median
+        :param in_pc: Input point cloud
+        :return: Point coordinates
+        """
         return 0.0, 0.0, 0.0
 
 
@@ -80,3 +116,5 @@ if __name__ == '__main__':
 
     alg = WAS_alg(in_data=input)
     alg.call()
+
+    print(f"PC characteristic:\n\t- Point: {alg.point}\n\t- Equation: {alg.equation}\n\t- Area: {alg.area}")
