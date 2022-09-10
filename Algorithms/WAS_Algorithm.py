61429684a28525e08f40a7f9875fdbe8ece8adf1
Daniil Fataliev <55330425+FelixFat@users.noreply.github.com>
2022-09-10 15:36:05 +0300

    MRG: Implementation of the WAS algorithm version 0.2 (#1)
    * FIX: Code commenting
    
    * FIX: WAS_alg version 0.1
    
    * FIX: WAS_alg version 0.2

--- Algorithms/WAS_Algorithm.py
+++ Algorithms/WAS_Algorithm.py
@@ -1,54 +1,81 @@
 import warnings
 
 import numpy as np
-import pyransac3d as pyrsc
+from sklearn.cluster import DBSCAN
+
+import Int_Library as lib
 
 
 class WAS_alg:
+    """
+    Weighted Area Search Algorithm for SLP search
+    """
+
     def __init__(self, in_data=np.array([])):
-        self.data = in_data
+        self.data_ = in_data
 
-        self.inliers = []
-        self.equation = []
+        self.inliers_ = np.array([])
+        self.equation_ = np.array([])
 
-        self.slope = 0.0
-        self.area = 0.0
-        self.score = 0.0
-        self.point = [0.0, 0.0, 0.0]
+        self.scale_ = 0.01
 
-    def call(self):
-        point_cloud = self.data.copy()
+        self.slope_ = 0.0
+        self.area_ = 0.0
+        self.score_ = 0.0
+        self.point_ = np.array([])
 
-        while len(point_cloud) >= 0.3 * len(self.data):
-            equation, inliers = self.plane_detection(in_pc=point_cloud)
-            slope = self.zone_slope(in_eq=equation)
+    def fit(self):
+        """
+        Weighted Area Search algorithm call
+        :return: None
+        """
 
-            clusters = self.zone_clustering(in_pc=inliers)
+        point_cloud = self.data_.copy()
+
+        while len(point_cloud) >= 0.3 * len(self.data_):
+            equation, inliers = self._plane_detection(in_pc=point_cloud)
+            slope = self._zone_slope(in_eq=equation)
+
+            cloud = np.array([point_cloud[i] for i in inliers])
+
+            clusters = self._zone_clustering(in_pc=cloud)
             for cluster in clusters:
-                area = self.zone_area(in_pc=cluster)
-                score = self.zone_estimate(in_area=area, in_slope=slope)
-                if score > self.score:
-                    self.point = self.point_definition(in_pc=cluster)
+                area = self._zone_area(in_pc=cluster)
+                score = self._zone_estimate(in_pc=cluster, in_area=area, in_slope=slope)
+
+                print(f">> Equation: {equation}; Slope: {slope}; Area: {area}; Score: {score}.")
+                if score > self.score_:
+                    self.inliers_ = cluster
+                    self.equation_ = equation
+                    self.slope_ = slope
+                    self.area_ = area
+                    self.score_ = score
+                    self.point_ = self._point_determination(in_pc=cluster)
             point_cloud = np.delete(point_cloud, inliers, axis=0)
 
-    def plane_detection(self, in_pc):
-        # RANSAC realization
-        plane = pyrsc.Plane()
-        equation, inliers = plane.fit(
-            pts=in_pc,
+        return None
+
+    def _plane_detection(self, in_pc):
+        """
+        Plane detection in point cloud with RANSAC algorithm
+        :param in_pc: Input point cloud
+        :return: Plane equation, Inliers points
+        """
+
+        equation, inliers = lib.RANSAC_plane(
+            cloud=in_pc,
             thresh=0.05,
-            minPoints=100,
-            maxIteration=1000
+            max_iter=1000
         )
 
         return equation, inliers
 
-    def zone_clustering(self, in_pc):
-        # Kd-tree + euclidean clustering
-        return []
-
-    def zone_slope(self, in_eq):
-        # Slope
+    def _zone_slope(self, in_eq):
+        """
+        Zone slope calculation
+        :param in_eq: Input equation of plane
+        :return: Zone slope
+        """
 
         # rad_phi = (a, b) / (magnitude(a) * magnitude(b))
         # grad_phi = abs(arccos(rad_phi) * 180 / PI)
@@ -62,21 +89,87 @@ class WAS_alg:
 
         return slope
 
-    def zone_area(self, in_pc):
-        return 0
+    def _zone_clustering(self, in_pc):
+        """
+        Zones clustering in point cloud with Kd-tree and Euclidean clustering algorithms
+        :param in_pc: Input point cloud
+        :return: Zones points clusters
+        """
 
-    def zone_estimate(self, in_area, in_slope):
-        return 0.0
+        model = DBSCAN(
+            eps=2,
+            min_samples=5,
+            metric='euclidean',
+            algorithm='kd_tree',
+            leaf_size=30
+        )
+        model.fit(X=in_pc)
 
-    def point_definition(self, in_pc):
-        # Geometric median
-        return 0.0, 0.0, 0.0
+        num_clusters = set(model.labels_)
+        if -1 in num_clusters:
+            num_clusters.remove(-1)
+
+        clusters = []
+        # NEED TO OPTIMIZE (calculations)
+        for num in num_clusters:
+            clusters.append(
+                np.array([
+                    in_pc[i]
+                    for i in range(len(model.labels_))
+                    if model.labels_[i] == num
+                ])
+            )
+
+        return clusters
+
+    def _zone_area(self, in_pc):
+        """
+        Zone area calculation
+        :param in_pc: Input point cloud
+        :return: Zone area
+        """
+
+        square = self.scale_ ** 2
+        area = len(in_pc) * square
+
+        return area
+
+    def _zone_estimate(self, in_pc, in_area, in_slope):
+        """
+        Landing zone estimation
+        :param in_area: Input zone area
+        :param in_slope: Input zone slope
+        :return: Estimate
+        """
+
+        # AREA_min = 0.126 m^2
+        # ANGLE_max = 15.0 grad
+        est = (np.sqrt((0.126 - in_area) ** 2) + np.sqrt((15.0 - in_slope) ** 2)) / 2
+
+        return est
+
+    def _point_determination(self, in_pc):
+        """
+        Landing point determination with geometric median
+        :param in_pc: Input point cloud
+        :return: Point coordinates
+        """
+
+        # NEED TO CHECK (linearized method)
+        g_median = np.array(lib.geometric_median(in_pc))
+
+        return g_median
 
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
 
     input = np.array([np.array([i, j, 0.5]) for j in range(100) for i in range(100)])
+    input[2] = [1000, -1000, 0.5]
+    input[5] = [-1000, -1001, 0.5]
+    input[10] = [-1001, -1000, 0.5]
 
     alg = WAS_alg(in_data=input)
-    alg.call()
+    alg.fit()
+
+    print(f"WAS algorithm result:\n\t- Point: {alg.point_}\n\t- Equation: {alg.equation_}\n\t- Area: {alg.area_}")
