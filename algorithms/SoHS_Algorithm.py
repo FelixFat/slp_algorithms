a71aa13684ca4bf6945a08e48157141787d55621
FelixFat <felixfat9@gmail.com>
2022-09-11 22:40:50 +0300

    FIX: SoHS Algorithm version 1.0

--- algorithms/SoHS_Algorithm.py
+++ algorithms/SoHS_Algorithm.py
@@ -19,19 +19,42 @@ class SoHS_alg:
         self.inliers_ = np.empty([0, 3])
 
         self.area_ = 0.0
-        self.deviation_ = 0.0
 
-        self.score_ = 0.0
+        self.score_ = np.empty([1, 1])
         self.point_ = np.empty([0, 3])
 
     def fit(self):
-        self.__pc_decomposition()
+        """
+        Squares of Heights Search Algorithm fit
+        :return: None
+        """
+
+        zoneL, zones = self.__pc_decomposition()
+        self.score_ = np.empty(zones.shape[:2])
+
+        soh = np.empty([0, 3])
+
+        for row in range(self.score_.shape[0]):
+            for col in range(self.score_.shape[1]):
+                self.score_[row, col] = self.__zone_estimate(zones[row, col])
+
+                if self.score_[row, col] is not None:
+                    flat = zones[row, col].transpose(2, 0, 1).reshape(3, -1)
+                    flat = np.array([point for point in zip(flat[0], flat[1], flat[2])])
+                    point = np.mean(flat, axis=0)
+
+                    soh = np.vstack([soh, point])
+                    self.inliers_ = np.vstack([self.inliers_, flat])
+
+        self.area_ = self.__zone_area(in_pc=soh, in_zoneL=zoneL)
+        self.point_ = self.__point_determination(in_pc=soh)
+
         return None
 
     def __pc_decomposition(self):
         """
         Point Cloud Decomposition
-        :return: Squares of heights
+        :return: L zone shape, Squares of heights
         """
 
         zoneL = lib.divide_and_conquer(shape=self.shape_.copy())
@@ -55,16 +78,36 @@ class SoHS_alg:
                 h_start, h_stop = col_i * zoneL[0], col_i * zoneL[0] + zoneL[0]
                 frame_zones[row_i, col_i] = frame[w_start:w_stop, h_start:h_stop]
 
-        return frame_zones
+        return zoneL, frame_zones
 
-    def __pc_estimation(self):
-        return 0.0
+    def __zone_estimate(self, in_pc):
+        """
+        Zone estimate calculation
+        :param in_pc: Input point cloud
+        :return: Point cloud variance
+        """
 
-    def __zone_estimation(self, in_pc):
-        return 0.0
+        height_flat = in_pc[:, :, 2].flatten()
 
-    def __soh_organization(self, in_pc):
-        return 0.0
+        diff_h = np.max(height_flat) - np.min(height_flat)
+        mean_h = np.mean(height_flat)
+        std_h = np.std(height_flat)
+
+        var_h = std_h / mean_h if diff_h <= 0.5 or std_h <= 0.1 else None
+
+        return var_h
+
+    def __zone_area(self, in_pc, in_zoneL):
+        """
+        Zone area calculation
+        :param in_pc: Input point cloud
+        :return: Zone area
+        """
+
+        square = (self.scale_ ** 2) * np.prod(in_zoneL)
+        area = len(in_pc) * square
+
+        return area
 
     def __point_determination(self, in_pc):
         """
@@ -90,5 +133,5 @@ if __name__ == '__main__':
     alg.fit()
     stop_time = time.time() - time_start
 
-    print(f"CDS algorithm result:\n\t- Point: {alg.point_}\n\t- Area: {alg.area_}\n\t- Deviation: {alg.deviation_}")
+    print(f"CDS algorithm result:\n\t- Point: {alg.point_}\n\t- Area: {alg.area_}")
     print(f"Time: {stop_time}")
